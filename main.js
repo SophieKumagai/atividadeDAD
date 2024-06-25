7d5feba930823fb09b3ff1c873a803a4869c0573
sophiekumagai <sophie.satie20@gmail.com>
2024-06-25 12:16:32 -0300

    alterando

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ bt.addEventListener('click', () => {
     const nome = localStorage.getItem("name")
     console.log(nome);
     if (nome == undefined) {
-        window.open('../pagina-inicial/index.html', '_self')
+        window.open('./pagina-inicial/index.html', '_self')
     } else {
-        window.open("../exercicio_1/index.html", "_self")
+        window.open("./exercicio_1/indexq1.html", "_self")
     }
 })
