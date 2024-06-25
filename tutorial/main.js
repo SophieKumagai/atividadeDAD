b1b022e4a518e63a2856396a12ccc16322576f2b
sophiekumagai <sophie.satie20@gmail.com>
2024-06-25 13:00:05 -0300

    alterando o tutorial

--- tutorial/main.js
+++ tutorial/main.js
@@ -18,7 +18,6 @@ txt3.innerHTML = "<p id='parte3'> Parabéns, Treinador Pokémon " + "<strong>"+n
 txt6.innerHTML = "<p id='parte6'> Pronto, Treinador Pokémom " + "<strong>"+nome.toUpperCase()+"</strong>" +"! Agora você sabe como usar consultas em <b>MongoDB</b> para encontrar informações sobre seus Pokémons, assim como faz com sua PC box! <b>Boa sorte em suas buscas</b>!</p>";
  
 function verificar() {
-    console.log("verificar "+cont)
     if (cont == 0) {
         txt1.classList.remove("invisivel")
         txt2.classList.add("invisivel")
@@ -56,9 +55,8 @@ function verificar() {
             txt3.classList.add("fadeInText")
             bt_pular.classList.add('invisivel')
         } else {
-            cont -= 1
             window.alert("Poxa, parece que algo deu errado. Tente novamente!")
-            verificar()
+            cont-=1
         }
     } else if (cont == 4) {
         txt3.classList.add("invisivel")
@@ -78,9 +76,8 @@ function verificar() {
             txt4.classList.add("fadeInText")
             bt_pular.classList.add('invisivel')
         } else {
-            cont -= 1
             window.alert("Poxa, parece que algo deu errado. Tente novamente!")
-            verificar()
+            cont-=1
         }
     } else if (cont == 6) {
         txt4.classList.add("invisivel")
@@ -100,9 +97,8 @@ function verificar() {
             txt5.classList.add("fadeInText")
             bt_pular.classList.add('invisivel')
         } else {
-            cont -= 1
             window.alert("Poxa, parece que algo deu errado. Tente novamente!")
-            verificar()
+            cont-=1
         }
     } else if (cont == 8) {
         txt5.classList.add("invisivel")
@@ -122,9 +118,8 @@ function verificar() {
             txt6.classList.add("fadeInText")
             bt_pular.classList.add('invisivel')
         } else {
-            cont -= 1
             window.alert("Poxa, parece que algo deu errado. Tente novamente!")
-            verificar()
+            cont-=1
         }
         bt_avancar.textContent = "Concluir"
     } else if (cont ==  10) {
@@ -133,11 +128,105 @@ function verificar() {
         window.open("../exercicio_1/indexq1.html", "_self")
     }
 }
+
+function verificarSemTexto() {
+    console.log("verificar "+cont)
+    if (cont == 0) {
+        txt1.classList.remove("invisivel")
+        txt2.classList.add("invisivel")
  
+        txt1.classList.add("fadeInText")
+        txt2.classList.remove("fadeInText")
+ 
+        bt_voltar.classList.add('desabilitado')
+    } else if (cont == 1) {
+        txt1.classList.add("invisivel")
+        txt2.classList.remove("invisivel")
+ 
+        txt1.classList.remove("fadeInText")
+        txt2.classList.add("fadeInText")
+ 
+        bt_voltar.classList.remove('desabilitado')
+ 
+        valor.classList.add("invisivel")
+        bt_pular.classList.add('invisivel')
+    } else if (cont == 2) {
+        txt2.classList.add("invisivel")
+        valor.classList.remove("invisivel")
+ 
+        valor.value = ""
+ 
+        txt3.classList.add("invisivel")
+        txt3.classList.remove("fadeInText")
+        bt_pular.classList.remove('invisivel')
+    }  else if (cont == 3) {
+        resposta = valor.value
+        valor.classList.add("invisivel")
+        txt3.classList.remove("invisivel")
+        txt3.classList.add("fadeInText")
+        bt_pular.classList.add('invisivel')
+    } else if (cont == 4) {
+        txt3.classList.add("invisivel")
+        valor.classList.remove("invisivel")
+ 
+        valor.value = ""
+ 
+        txt4.classList.add("invisivel")
+        txt4.classList.remove("fadeInText")
+        bt_pular.classList.remove('invisivel')
+ 
+    }  else if (cont == 5) {
+        resposta = valor.value
+        valor.classList.add("invisivel")
+        txt4.classList.remove("invisivel")
+        txt4.classList.add("fadeInText")
+        bt_pular.classList.add('invisivel')
+    } else if (cont == 6) {
+        txt4.classList.add("invisivel")
+        valor.classList.remove("invisivel")
+ 
+        valor.value = ""
+ 
+        txt5.classList.add("invisivel")
+        txt5.classList.remove("fadeInText")
+        bt_pular.classList.remove('invisivel')
+    }else if (cont == 7) {
+        bt_pular.classList.remove('invisivel')
+        resposta = valor.value
+        valor.classList.add("invisivel")
+        txt5.classList.remove("invisivel")
+        txt5.classList.add("fadeInText")
+        bt_pular.classList.add('invisivel')
+    } else if (cont == 8) {
+        txt5.classList.add("invisivel")
+        valor.classList.remove("invisivel")
+ 
+        valor.value = ""
+ 
+        txt6.classList.add("invisivel")
+        txt6.classList.remove("fadeInText")
+        bt_pular.classList.remove('invisivel')
+
+        bt_avancar.textContent = "Avançar"
+    }  else if (cont == 9) {
+        bt_pular.classList.remove('invisivel')
+        resposta = valor.value
+        valor.classList.add("invisivel")
+        txt6.classList.remove("invisivel")
+        txt6.classList.add("fadeInText")
+        bt_pular.classList.add('invisivel')
+        bt_avancar.textContent = "Concluir"
+    } else if (cont ==  10) {
+        window.alert("Parabéns, você concluiu o tutorial!")
+        localStorage.setItem("tutorial", "entrou")
+        window.open("../exercicio_1/indexq1.html", "_self")
+    }
+}
+
 function voltar() {
-    if (cont>=0) {
+    if (cont>0) {
         cont -= 1
-        verificar()
+        verificarSemTexto()
     }
 }
  
@@ -145,15 +234,20 @@ function avancar() {
     cont += 1
     verificar()
 }
+
+function pular() {
+    cont += 1
+    verificarSemTexto()
+}
  
-bt_avancar.addEventListener("click", (e) => {
-    console.log("avancar");
-    console.log(e.target);
+bt_avancar.addEventListener("click", () => {
     avancar()
 })
  
-bt_voltar.addEventListener("click", (e) => {
-    console.log("voltar");
-    console.log(e.target);
+bt_voltar.addEventListener("click", () => {
     voltar()
+})
+
+bt_pular.addEventListener("click", () => {
+    pular()
 })
